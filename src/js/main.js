8cc45a63ce9721a10368db49d5a62058e1c002fd
Linaida <dev.linda66@gmail.com>
2014-07-26 17:39:42 +0200

    Commit avant de passer au projet Cake

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,38 @@
-$(function () {
-    var couleur = "blanc";
+var couleur = "blanc";
+
+Sortable.create(
+    $('liste_couleur'),{
+        tag: 'li',
+        overlap: 'horizontal',
+        constraint: false,
+        onDrag : function(e){
+            couleur = e.css('background-color');
+        }
+    }
+);
+Droppables.add('bille',{
+   accept:'liste_couleur',
+    onDrop: function(element){
+        element.setStyle('background-color',couleur);
+    }
+});
+    /*
+
     $('btn_start').on('click', function (e) {
         $('#testBlock').removeClass('hide');
 
     });
+
     $$('.btn_valider').first().on('click', function (e) {
         $('div_bille').appendChild($('testBlock').clone(true));
     });
+*/
+
+/*
 
     $$('.couleur').each(function (element) {
-        element.draggable({
-            appendTo: "body",
-            revert: true
-        })
+       var ma_couleur =new Draggable(element.readAttribute('id'),{revert: true});
+
 
     });
 
@@ -33,5 +53,4 @@ $(function () {
 
         })
     });
-
-});
+ */
